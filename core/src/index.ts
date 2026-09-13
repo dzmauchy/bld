f6084d65f4b3ef91393698d71c12eb185481d65e
demi <dzmiter.auchynnikau@gmail.com>
2026-09-13 12:26:49 -0300

    BLD-00019 Restore the original blocks.ts

--- core/src/index.ts
+++ core/src/index.ts
@@ -5,7 +5,6 @@ export function add(left: number, right: number): number {
 export * from "./types/index";
 export * from "./model/index";
 export * as ri from "./ri/index";
-export { RiExecutionContext, RiProgram } from "./ri/index";
 export { modelAssetFiles } from "./modelAssets";
 export {
   CoreSchemaCatalog,
--- core/src/ri/index.ts
+++ core/src/ri/index.ts
@@ -1,4 +1,2 @@
 export * from "./context";
 export * from "./blocks";
-export * from "./runtime";
-export * from "./program";
